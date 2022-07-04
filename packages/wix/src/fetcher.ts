3be74c3077a0439f61ba3bf1579e4f906b4e972a
Ronny Ringel <ronnyr@wix.com>
2022-07-04 15:57:19 +0300

    logs

--- packages/wix/src/fetcher.ts
+++ packages/wix/src/fetcher.ts
@@ -20,7 +20,7 @@ const fetcher: any = async ({
       })
     const json = await res.json()
     accessToken = json.accessToken
-    console.log(json)
+    console.log(json, WIX_DOMAIN)
     Cookies.set(WIX_ACCESS_TOKEN_COOKIE, accessToken!)
   }
 
