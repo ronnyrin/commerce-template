d94111fed53934167ad6256e1896dde938db2b82
Ronny Ringel <ronnyr@wix.com>
2022-07-18 18:54:55 +0300

    logic hack

--- packages/wix/src/api/endpoints/login/login.ts
+++ packages/wix/src/api/endpoints/login/login.ts
@@ -1,6 +1,8 @@
 import type { LoginEndpoint } from '.'
 import { WIX_ACCESS_TOKEN_COOKIE, WIX_CUSTOMER_TOKEN_COOKIE, WIX_COOKIE_EXPIRE } from '../../../const'
 
+const cache = {email: '', password: ''};
+
 const login: LoginEndpoint['handlers']['login'] = async ({
   res,
   body: { email, password },
@@ -8,7 +10,7 @@ const login: LoginEndpoint['handlers']['login'] = async ({
   config,
   // commerce
 }) => {
-  if (!(email && password)) {
+  if (!(email && password) && !(cache.email && cache.password)) {
     return res.status(400).json({
       data: null,
       errors: [{ message: 'Invalid request' }]
@@ -17,18 +19,24 @@ const login: LoginEndpoint['handlers']['login'] = async ({
 
   try {
     const accessToken = cookies[WIX_ACCESS_TOKEN_COOKIE]
-    const response = await fetch('http://www.wix.com/_api/wix-sm/v1/auth/login?petri_ovr=specs.ShouldForceCaptchaVerificationOnSignupSpec:Disabled', {
+    const response = await fetch('http://www.wix.com/_api/wix-sm/v1/auth/login', {
       headers: {
         'Authorization': accessToken!,
         'Content-Type': 'application/json'
       },
       method: 'POST',
       body: JSON.stringify({
-        email,
-        password,
+        email: email || cache.email,
+        password: password || cache.password,
       })
     })
     const data = await response.json()
+
+    if (data.session?.token) {
+      cache.email = email || cache.email;
+      cache.password = password || cache.password;
+    }
+
     res.setHeader(
       'Set-Cookie',
       [`${WIX_CUSTOMER_TOKEN_COOKIE}=${data.session.token}; Max-Age=${WIX_COOKIE_EXPIRE*86400}; Path=/`, `${WIX_ACCESS_TOKEN_COOKIE}=; expires=Thu, Jan 01 1970 00:00:00 UTC;; Path=/`]
--- packages/wix/src/cart/use-cart.tsx
+++ packages/wix/src/cart/use-cart.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import useCommerceCart, { UseCart } from '@vercel/commerce/cart/use-cart'
-import { normalizeCart } from '../utils'
+import { normalizeCart, getCustomerToken } from '../utils'
 import Cookies from 'js-cookie'
 
 import {
@@ -19,11 +19,15 @@ export const handler: any = {
       const { cart } = await fetch({
         ...options,
       })
+
       const { checkoutId } = await fetch({
-        method: 'POST',
         url: `ecom/v1/carts/${cart.id}/create-checkout`,
         variables: JSON.stringify({channelType: 'WEB'})
       })
+
+      if (getCustomerToken()) {
+        await fetch({url: '/api/login'});
+      }
 
       return normalizeCart({
         cart,
