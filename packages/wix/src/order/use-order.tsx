549ce3c8b6a1ad04dd507307d3193dc0d3e8dc60
Ronny Ringel <ronnyr@wix.com>
2022-07-20 14:04:19 +0300

    fix order

--- packages/wix/src/order/use-order.tsx
+++ packages/wix/src/order/use-order.tsx
@@ -1,5 +1,7 @@
 import useCommerceOrder, { UseOrder } from '@vercel/commerce/order/use-order'
 import { normalizeOrder } from '../utils'
+import Cookies from 'js-cookie'
+import { WIX_CART_ID_COOKIE, WIX_CHECKOUT_ID_COOKIE } from '../const'
 export default useCommerceOrder as UseOrder<typeof handler>
 
 export const handler: any = {
@@ -8,6 +10,10 @@ export const handler: any = {
     method: 'GET',
   },
   async fetcher({ options, fetch, input }: any) {
+    if (!input.orderId) {
+      return;
+    }
+
     const { order } = await fetch({
       ...options,
       url: `/api/order?orderId=${input.orderId}`
@@ -18,6 +24,8 @@ export const handler: any = {
   useHook:
     ({ useData }: any) =>
       (input: any) => {
+        Cookies.remove(WIX_CHECKOUT_ID_COOKIE)
+        Cookies.remove(WIX_CART_ID_COOKIE)
         return useData({
           input: [
             ['orderId', input?.orderId],
